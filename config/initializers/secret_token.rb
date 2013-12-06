2dd94ace72714640d5e0f5fb45451fbc2d3ef52f
Toby Marsden <toby@toby.org.uk>
2013-12-06 19:39:11 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Campaignkit::Application.config.secret_key_base = '9e53ec446d5c94247c9a00001189a679aa0d783284ec84785a27f0f25e479f890b1c2ca6133ccf61dfb82f51a317257dc3e819c35f4a172a8da655026586ee95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Campaignkit::Application.config.session_store :cookie_store, key: '_campaignkit_session'
